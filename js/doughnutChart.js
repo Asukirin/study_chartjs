728cde18c9e19f2976563f7fce405114c0bd5266
Asukirin <asukiri.liar@gmail.com>
2020-05-04 10:26:57 +0900

    first commit

---
+++ js/doughnutChart.js
@@ -0,0 +1,70 @@
+Chart.plugins.unregister(ChartDataLabels);
+
+let members = 50;
+let visitor = 50;
+
+
+var dataLabelPlugin = {};
+
+
+var dctx = document.getElementById('doughnutChart').getContext('2d');
+dctx.canvas.height = 320;
+
+var doughnutChart = new Chart(dctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['メンバー', 'ビジター'],
+        datasets: [{
+            data: [members, visitor],
+            backgroundColor: [
+                "#87cefa",
+                "#f0f8ff"
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        cutoutPercentage: 80,
+        title: {
+            display: true,
+            text: 'サポート割合',
+            position: 'bottom'
+        }
+    }
+});
+
+
+var hctx = document.getElementById('horizontalBarChart').getContext('2d');
+hctx.canvas.height = 80;
+
+let targetValue = 800000;
+
+var horizontalBarChart = new Chart(hctx, {
+    type: 'horizontalBar',
+    data: {
+        datasets: [{
+            data: [members],
+            backgroundColor: [
+                "#87cefa",
+            ],
+            borderWidth: 0
+        }]
+    },
+    options: {
+        responsive: true,
+        barPercentage: 0.7,
+        categoryPercentage: 0.7,
+        maxBarThicknes: 40,
+        legend: {
+            display: false
+        },
+        scales: {
+            yAxes: [{ display: false }],
+            xAxes: [{ display: false }]
+        }
+    },
+    plugins: [dataLabelPlugin]
+});
+
